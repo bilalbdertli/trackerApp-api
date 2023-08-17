83f62ba7c557360fcc5cb42f5e76ca81037dc164
Bilal Dertli <127341202+bilalbdertli@users.noreply.github.com>
2023-08-17 21:21:50 +0300

    adding dto and mapper classes
    they will be used for transferring data between layouts in a simpler way

--- src/main/java/com/example/todoappapi/Repo/UserRepo.java
+++ src/main/java/com/example/todoappapi/Repo/UserRepo.java
@@ -5,4 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepo extends MongoRepository<User, String> {
     public User findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+
+    void deleteByUsername(String username);
 }
---
+++ src/main/java/com/example/todoappapi/dto/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.todoappapi.dto;
+
+import com.example.todoappapi.Model.User;
+
+import java.util.Objects;
+
+public class UserMapper {
+    public static UserDto mapToDto(User user) {
+        UserDto userDto = new UserDto();
+        if(Objects.isNull(user)) {return userDto;}
+        userDto.setUsername(user.getUsername());
+        userDto.setPassword(user.getPassword());
+        userDto.setRole(user.getRole());
+        return userDto;
+    }
+}
